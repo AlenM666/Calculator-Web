fda122f725759a721747528a08c33991a6558512
AlenM666 <llolek278@gmail.com>
2024-06-07 23:15:23 +0200

    testing repo

---
+++ Calculator/src/main/java/loki/Calculator/CalculatorController.java
@@ -0,0 +1,11 @@
+package loki.Calculator;
+
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("api/calculator")
+public class CalculatorController {
+
+}
